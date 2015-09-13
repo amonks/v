7064ee2472db6577b51d06ece346a079767619d7
Andrew Monks <a@monks.co>
2015-09-13 00:27:12 -0500

    spacing

--- main.js
+++ main.js
@@ -10,12 +10,15 @@ require(
     'use strict'
 
     let control_window = new ExternalWindow('control_window')
+
     control_window.on('ready', function () {
       let ui = new Pane('controls')
       control_window.add(ui)
+
       ui.on('ready', function () {
         let panel = new Panel()
         ui.add(panel)
+
         panel.on('ready', function () {
           let text = new StringControl('text', {initial: 'some text'})
           panel.add(text)
@@ -24,9 +27,11 @@ require(
     })
 
     let primary_window = new PrimaryWindow()
+
     primary_window.on('ready', function () {
       let ui = new Pane('output')
       primary_window.add(ui)
+
       ui.on('ready', function () {
         ui.add($('<h1>Hello!</h1>'))
       })
