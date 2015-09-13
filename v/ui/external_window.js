2d7655bb4ec3acb62561edf9acfa825085618a7e
Andrew Monks <a@monks.co>
2015-09-12 22:43:02 -0500

    created empty

--- v/ui/external_window.js
+++ v/ui/external_window.js
@@ -6,7 +6,7 @@ define(['v/ui/base', 'jquery'], function (BaseUI, $) {
   let ExternalWindow = function (id) {
     BaseUI.call(this)
 
-    this.window = window.open('empty', id)
+    this.window = window.open('v/empty', id)
 
     let that = this
     this.window.onload = function () {
